5b76452ac5ed964e5bea3c9833453d621e2f8199
Jake Soenneker <jake@soenneker.com>
2024-07-08 08:52:54 -0500

    test misc

--- test/GitHubClientUtilTests.cs
+++ test/GitHubClientUtilTests.cs
@@ -1,7 +1,11 @@
+using System.Threading;
 using Soenneker.GitHub.Client.Abstract;
 using Soenneker.Tests.FixturedUnit;
+using System.Threading.Tasks;
 using Xunit;
 using Xunit.Abstractions;
+using FluentAssertions;
+using Soenneker.Facts.Local;
 
 namespace Soenneker.GitHub.Client.Tests;
 
@@ -13,5 +17,13 @@ public class GitHubClientUtilTests : FixturedUnitTest
     public GitHubClientUtilTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
     {
         _util = Resolve<IGitHubClientUtil>();
+    }
+
+    [LocalFact]
+    public async Task Get_should_get()
+    {
+        var client = await _util.Get(CancellationToken.None);
+        client.Should().NotBeNull();
+       
     }
 }
