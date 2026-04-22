f34e572e8e8e1981590134dd87017609245a5fb6
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:03:13 -0500

    TUnit conversion

--- test/Soenneker.GitHub.Client.Tests/GitHubClientUtilTests.cs
+++ test/Soenneker.GitHub.Client.Tests/GitHubClientUtilTests.cs
@@ -2,43 +2,42 @@ using System.Threading.Tasks;
 using AwesomeAssertions;
 using Octokit;
 using Soenneker.GitHub.Client.Abstract;
-using Soenneker.Tests.FixturedUnit;
-using Xunit;
+using Soenneker.Tests.HostedUnit;
 
 namespace Soenneker.GitHub.Client.Tests;
 
-[Collection("Collection")]
-public class GitHubClientUtilTests : FixturedUnitTest
+[ClassDataSource<Host>(Shared = SharedType.PerTestSession)]
+public class GitHubClientUtilTests : HostedUnitTest
 {
     private readonly IGitHubClientUtil _util;
 
-    public GitHubClientUtilTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    public GitHubClientUtilTests(Host host) : base(host)
     {
         _util = Resolve<IGitHubClientUtil>(true);
     }
 
-    [Fact]
+    [Test]
     public async ValueTask Get_should_get_client()
     {
         GitHubClient client = await _util.Get("blah", CancellationToken);
         client.Should().NotBeNull();
     }
 
-    [Fact]
+    [Test]
     public async ValueTask Get_should_get_client_with_null()
     {
         GitHubClient client = await _util.Get(null!, CancellationToken);
         client.Should().NotBeNull();
     }
 
-    [Fact]
+    [Test]
     public async ValueTask Get_should_get_client_with_empty()
     {
         GitHubClient client = await _util.Get("", CancellationToken);
         client.Should().NotBeNull();
     }
 
-    [Fact]
+    [Test]
     public async ValueTask Get_should_get_client_without_token()
     {
         GitHubClient client = await _util.Get(CancellationToken);
